0d5c976f791328c23cc9aade66cfcfd9166b3a1f
Ayan Banerjee <ayanbanerj@gmail.com>
2025-04-18 15:32:43 -0400

    added functions to utilities.py

--- utilities.py
+++ utilities.py
@@ -1,41 +1,33 @@
-import numpy as np
-from langchain_community.embeddings import HuggingFaceEmbeddings
-from langchain_community.vectorstores import FAISS
-from langchain_experimental.text_splitter import SemanticChunker
-import torch
-from gliner import GLiNER
-from langchain_experimental.text_splitter import SemanticChunker
-import re
-
-# Use Microsoft E5 model instead of MPNet
-model_kwargs = {'device': 'cuda' if torch.cuda.is_available() else 'cpu'}
-encode_kwargs = {'normalize_embeddings': True}  # does l2 norm for the cos sim
-modelemb = HuggingFaceEmbeddings(
-    model_name="intfloat/e5-base-v2", 
-    model_kwargs=model_kwargs,
-    encode_kwargs=encode_kwargs,
-)
-
-
-# ============ Setup Models ============
-
-# Load GLiNER for NER
-DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-gliner_model = GLiNER.from_pretrained(
-    "glinermodellocal",
-    local_files_only=False
-)
-gliner_model.config.max_len = 512
-gliner_model.to(DEVICE)
-
-
-
-# ============ Configuration ============
-labels = ["date", "location", "person", "action", "finance", "legal", "event", "product", "organization"]
-
-# ============ Helper Functions ============
+import re # being used in clean_text function
 from dateutil import parser
 
+
+"""
+
+"""
+def clean_text(text: str) -> str:
+    """
+    Clean text while preserving useful characters:
+    - Removes weird/unprintable symbols
+    - Keeps letters, numbers, basic punctuation: @ . , ? : ; ! _ ( ) &
+    - Normalizes whitespace
+    """
+
+    # lowercase the texts 
+
+    text.lower()
+    # Remove anything not in the allowed set
+    text = re.sub(r"[^a-z0-9@.,?;:!()&\/_ ]", '', text)
+    
+    # Normalize whitespace
+    text = re.sub(r"\s+", " ", text)
+    
+    return text.strip()
+
+
+"""
+
+"""
 def parse_email_date(date_tokens: List[str]) -> str:
     raw_date_str = " ".join(date_tokens)
     try:
@@ -45,7 +37,8 @@ def parse_email_date(date_tokens: List[str]) -> str:
         print(f"Date parse error: {e}")
         return "unknown"
 
-
+"""
+"""
 def extract_email_metadata(msg, df_idx):
     split_msg = msg.split()
     metadata = {}
